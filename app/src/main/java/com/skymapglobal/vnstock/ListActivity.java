750722192b56ee3b07ac6c671df45725692d9d2e
hungtrn75 <hungtrn75@gmail.com>
2023-03-09 17:50:45 +0700

    init search view

--- app/src/main/java/com/skymapglobal/vnstock/ListActivity.java
+++ app/src/main/java/com/skymapglobal/vnstock/ListActivity.java
@@ -1,8 +1,15 @@
 package com.skymapglobal.vnstock;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.SearchView.OnQueryTextListener;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -63,6 +70,33 @@ public class ListActivity extends AppCompatActivity {
           refreshLayout.setRefreshing(false);
         }));
     mCompositeDisposable.add(disposable);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu, menu);
+    MenuItem searchItem = menu.findItem(R.id.action_search);
+    Drawable drawable = searchItem.getIcon();
+    if (drawable != null) {
+      drawable.mutate();
+      drawable.setColorFilter(ContextCompat.getColor(this, R.color.white),
+          PorterDuff.Mode.SRC_ATOP);
+    }
+    SearchView searchView = (SearchView) searchItem.getActionView();
+    searchView.setQueryHint("Nhập mã chứng khoán cần tìm...");
+
+    searchView.setOnQueryTextListener(new OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String query) {
+        return false;
+      }
+
+      @Override
+      public boolean onQueryTextChange(String newText) {
+        return false;
+      }
+    });
+    return super.onCreateOptionsMenu(menu);
   }
 
   @Override
